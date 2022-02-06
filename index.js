3711545598e7c0b075c77c012fe8dd43cddbf5a5
Natividad Rivera <nat@natrivera.com>
2022-02-06 10:35:14 -0800

    many changes

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var worder = "java";
 var wrong = [];
 var array = [];
 var arr2 = [];
-var count = 0;
+var count = 1;
+var guess = 1;
 var words;
+var won = '';
+var actualword = 'BROWN';
+var yellow = '#c9b458';
+var darkyellow = '#b59f3b';
+var green = '#6aaa64';
+var darkgreen = '#538d4e';
+var gray = '#86888a';
+var black = '#212121';
 
 
 
@@ -23,18 +32,13 @@ function getWord() {
 
 }
 
-function newGame() {
-
-}
-
-
 
 $("document").ready(function() {
   getWord();
 });
 
 
-function clear() {
+function clear_unused() {
   count = 0;
   wrong = [];
   array = [];
@@ -73,97 +77,102 @@ function clear() {
 }
 
 $(".piece").click(function() {
-  var pid = $(this).attr("id").toUpperCase();
-  submit(pid);
-}); //end of submit
+  var p = $(this).attr("id").toUpperCase();
+  submitletter(p);
+}); //end of click letter
 
+function submitletter(p) {
+  if(count<6 & guess < 7 & won == '') {
+      letterid = '#' + guess + '' + count;
+      $(letterid).html(p);
+      count++;
+  }
+}// end of submitletter
 
-function submit(p) {
-  if (count < 7) {
-    check(p);
+$("#clear").click(function() {
+  clearguess();
+}); //end of click clear
+
+function clearguess() {
+  if(won=='' & count > 1) {
+    temp = count-1;
+    $('#' + guess+temp).html('');
+    count--;
   }
 }
 
-function check(d) {
-  if (d.length > 1) {
-    $("#guess").val("");
-  } else if (/^[a-zA-Z]/.test(d)) {
-    if (worder.indexOf(d) !== -1) {
-      $("#guess").val("");
-      array.push(d);
-      $("#filler").html("");
-      for (var g = 0; g < array.length; g++) {
-        for (var j = 0; j < worder.length; j++) {
-          var c = worder.substring(j, j + 1);
-          if (c === array[g]) {
-            if (/[A-Z]/.test(arr2[j]) === false) {
-	      $("#" + d).css("background-color" , "#87CEEB");
-              arr2[j] = d;
-            }
-          } else {
-            if (/[A-Z]/.test(arr2[j]) === false) {
-              arr2[j] = "_";
-            }
-          }
-        }
-      }
-      var str = arr2.join("");
-      $("#filler").html(str);
-    } else {
-      if (wrong.indexOf(d) !== -1) {
-	$("#" + d).css("background-color" , "transparent");
-        setTimeout(function() {
-          $("#" + d).css("background" , "red");
-        }, 400)
-      } else {
-        wrong.push(d);
-        count++;
-        $("#" + d).css("background-color", "red");
-        $("#pics").html(
-          "<img class='img-responsive' src='http://natrivera.com/hangman/img/" +
-            (count - 1) +
-            ".png'></img>"
-        );
-        if (count === 7) {
-          $("#message").html("Out of Guesses<br>Word: <em>" + worder + "</em>");
+$("#Enter").click(function() {
+  enterguess();
+}); //end of click Enter
+
+function enterguess() {
+  if (guess < 7 & count == 6 & won == '') {
+
+    var l1 = $('#' + guess + 1).html();
+    var l2 = $('#' + guess + 2).html();
+    var l3 = $('#' + guess + 3).html();
+    var l4 = $('#' + guess + 4).html();
+    var l5 = $('#' + guess + 5).html();
+
+    var guessword = l1 + l2 + l3 + l4 + l5;
+
+    if (checkwordinlist(guessword)) {// word is in list
+      //
+      if (guessword == actualword) {//winner -- guess is correct
+        colortiles('');
+        won = 'Y';
+      } else {// guess was incorrect
+
+        if (guess < 6) {// guesses still available
+          colortiles(l1,l2,l3,l4,l5,guessword);
+          guess++;
+          count = 1;
+        } else {// out of guesses
+          console.log('out of guesses');
         }
       }
     }
+
   } else {
-    $("#guess").val("");
+    //word not in list -- not a valid entry
   }
-  var temp = $("#filler").html();
-  if(worder === temp) {
-      $("#message").html("Winner!!!<br>Word: <em>" + worder + "</em>");
-      $("#filler").css("background-color" , "rgba(255,255,0,.1)");
-      count = 9;
-     }
-} //end of check
+}// end of enterguess
 
-function fill(v) {
-  var length = v.length;
-  $("#filler").html("");
-  for (var i = 0; i < length; i++) {
-    $("#filler").append("_");
+//function to check if the guess in a valid word
+function checkwordinlist() {
+  return true;
+}
 
+//function to color tiles of a guess
+function colortiles(b1,b2,b3,b4,b5,b) {
+  var colors = [];
+  if (b1=='') {//winner all green
+    $('#' + guess + 1).css("background-color" , darkgreen);
+    $('#' + guess + 2).css("background-color" , darkgreen);
+    $('#' + guess + 3).css("background-color" , darkgreen);
+    $('#' + guess + 4).css("background-color" , darkgreen);
+    $('#' + guess + 5).css("background-color" , darkgreen);
+  } else {// guess was wrong color the tiles appropriately
+    var position = 1;
+    var actuals =  actualword.split('');
+    [b1,b2,b3,b4,b5].forEach((x, i) => {
+      var tempcolor;
+      if (actuals.includes(x, 0)) {
+        tempcolor = darkyellow;
+        if(actuals[i] == x) {
+          tempcolor = green;
+        }
+      } else {
+        tempcolor = gray;
+      }
+      colors.push(tempcolor);
+      position++;
+    });
+    $('#' + guess + 1).css("background-color" , colors[0]);
+    $('#' + guess + 2).css("background-color" , colors[1]);
+    $('#' + guess + 3).css("background-color" , colors[2]);
+    $('#' + guess + 4).css("background-color" , colors[3]);
+    $('#' + guess + 5).css("background-color" , colors[4]);
   }
-} //end of fill
-
-function RandomWord() {
-
-  $.getJSON(wordapi, function(word) {
-    var num = word.words.length;
-    var random = Math.floor(Math.random() * num);
-    worder = word.words[random].toUpperCase();
-    console.log(worder);
-  }); //end of getJSON
-} //end of random word
-
-document.addEventListener('keydown', function(event) {
-  var x = event.which || event.keyCode;
-  var letter = String.fromCharCode(x);
-  submit(letter);
-  if(x == 13) {
-    newGame();
-  }
-}, true);
+  //console.log(colors);
+}
